1891b661f82baa0a4c028bad686e479c7896e70d
nickgran321 <55055772+nickgran321@users.noreply.github.com>
2020-03-17 00:49:22 -0400

    Initial Commit

---
+++ PresidentsListExpand/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PresidentsListExpand
+//
+//  Created by Nicholas Grant on 3/17/20.
+//  Copyright © 2020 nicholasgrant. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
